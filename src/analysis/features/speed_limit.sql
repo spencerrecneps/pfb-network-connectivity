dadfe8c8ab71edf8f5c93dd0658d3046a53a16c8
Spencer Gardner <spencergardner@gmail.com>
2017-06-23 17:59:24 -0500

    Add conversion for speed limit from kph to mph

--- src/analysis/features/speed_limit.sql
+++ src/analysis/features/speed_limit.sql
@@ -11,7 +11,7 @@ WHERE   neighborhood_ways.osm_id = osm.osm_id
 AND     osm.maxspeed LIKE '% mph';
 
 UPDATE  neighborhood_ways
-SET     speed_limit = osm.maxspeed::INT
+SET     speed_limit = (osm.maxspeed::INT * 0.621371)::INT
 FROM    neighborhood_osm_full_line osm
 WHERE   neighborhood_ways.osm_id = osm.osm_id
 AND     osm.maxspeed NOT LIKE '%mph%';
