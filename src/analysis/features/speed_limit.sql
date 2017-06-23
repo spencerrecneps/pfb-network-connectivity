8496206c8b279a683a423dd24ad895e8d9c35508
Spencer Gardner <spencergardner@gmail.com>
2017-06-23 17:54:28 -0500

    Add handling of non-mph speeds

--- src/analysis/features/speed_limit.sql
+++ src/analysis/features/speed_limit.sql
@@ -9,3 +9,9 @@ SET     speed_limit = substring(osm.maxspeed from '\d+')::INT
 FROM    neighborhood_osm_full_line osm
 WHERE   neighborhood_ways.osm_id = osm.osm_id
 AND     osm.maxspeed LIKE '% mph';
+
+UPDATE  neighborhood_ways
+SET     speed_limit = osm.maxspeed::INT
+FROM    neighborhood_osm_full_line osm
+WHERE   neighborhood_ways.osm_id = osm.osm_id
+AND     osm.maxspeed NOT LIKE '%mph%';
