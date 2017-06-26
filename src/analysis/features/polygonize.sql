e207f1213dae36df5b3812b93f312a8db82ae7b2
Spencer Gardner <spencergardner@gmail.com>
2017-06-26 07:46:22 -0500

    add blockid10

--- src/analysis/features/polygonize.sql
+++ src/analysis/features/polygonize.sql
@@ -33,3 +33,27 @@ USING   neighborhood_boundary AS nb
 WHERE   NOT ST_Intersects(cb1.geom,nb.geom);
 DELETE FROM neighborhood_census_blocks WHERE ST_Area(geom) < 1000;
 DELETE FROM neighborhood_census_blocks WHERE ST_Area(geom) * 2 / ST_Perimeter(geom) < 15;
+
+UPDATE  neighborhood_census_blocks
+SET     blockid10 = rpad(id::TEXT,15,'0');
+
+
+------------------------------------------
+-- copy to jobs
+------------------------------------------
+INSERT INTO neighborhood_census_block_jobs (geom,blockid10,jobs)
+SELECT  geom,
+        blockid10,
+        100
+FROM    neighborhood_census_blocks;
+
+
+------------------------------------------
+-- indexes
+------------------------------------------
+CREATE INDEX sidx_nbhdcb_geom ON neighborhood_census_blocks USING GIST (geom);
+CREATE INDEX idx_nbhdcb_blockid10 ON neighborhood_census_blocks (blockid10);
+CREATE INDEX sidx_nbhdcbj_geom ON neighborhood_census_block_jobs USING GIST (geom);
+CREATE INDEX idx_nbhdcbj_blockid10 ON neighborhood_census_block_jobs (blockid10);
+ANALYZE neighborhood_census_blocks;
+ANALYZE neighborhood_census_block_jobs;
